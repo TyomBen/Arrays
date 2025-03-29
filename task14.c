565e190e7962689cef56ba9ecd387231b742c77c
TyomBen <artyombenikyan@mail.ru>
2025-03-30 01:37:16 +0400

    I've written task

--- task14.c
+++ task14.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-#der arvac che
-int main () 
-{
-    
-}
+
+int main ()
+   {
+       char arr[4];
+  
+       for (int i = 0; i < 4; ++i){
+               scanf("%c", &arr[i]);
+          }
+ 
+       for (int i = 4; i >= 0; --i){
+              printf("%c", arr[i]);
+          }
+        printf("\n");  
+  }
+
